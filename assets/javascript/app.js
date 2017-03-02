009c2989ffcfca55f8bbd45311c1f58d62096327
Nicholas Heneisen <Nicholas Heneisen>
2017-03-02 10:39:24 -0500

    working on timer function, currently broken. Added more to html structure, creating ids for questions, answers, and timer to be pushed to.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,26 @@
+jQuery(document).ready(function($){
+console.log("Loadup");
+
 var game = {
+	//basic variables
 	correct: 0,
 	incorrect: 0,
-	timer: 30,
+	questionNum: 1,
 
-}
+
+	//timer variables and funtions
+	count: 30,
+	},
+	
+
+	//questions and answers for quiz
+
+
+
+
+}; //end game object
+
+console.log(game.count);
+
+
+}); //end jquery wrapper
